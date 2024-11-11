7657a69d72051346d35e25433f067db8e54084ec
tawcent <rusx745@gmail.com>
2024-11-11 21:51:01 +0400

    init maven

---
+++ src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,33 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIterator implements Iterator<Integer> {
+    private final int[][] data;
+    private int row;
+    private int column;
+
+    public MatrixIterator(int[][] data) {
+        this.data = data;
+        this.row = 0;
+        this.column = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (row < data.length && column >= data[row].length) {
+            row++;
+            column = 0;
+        }
+        return row < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[row][column++];
+    }
+}
