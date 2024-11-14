d5f994fa052746ed3602527582d43c07e390e3f1
tawcent <rusx745@gmail.com>
2024-11-14 21:53:41 +0400

    init maven

---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,52 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics generics = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        generics.printObject(first);
+        generics.printObject(second);
+        generics.printObject(third);
+        System.out.println();
+
+        generics.printBoundedWildCard(second);
+        generics.printBoundedWildCard(third);
+        System.out.println();
+        /* Ограничен сверху, только для Predator */
+
+        generics.printLowerBoundedWildCard(first);
+        generics.printLowerBoundedWildCard(second);
+        /* Ограничен снизу, только для Predator и его подклассов */
+    }
+
+    public void printObject(List<?> list) {
+        for (Iterator<?> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Predator> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Iterator<? super Predator> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
