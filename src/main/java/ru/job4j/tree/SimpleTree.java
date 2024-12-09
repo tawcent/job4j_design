0065cfe0858140c85e454adc8a9e1208ee7cce97
tawcent <rusx745@gmail.com>
2024-12-09 23:44:19 +0400

    init maven

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,46 @@
+package ru.job4j.tree;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        if (findBy(child).isPresent()) {
+            return false;
+        }
+
+        Optional<Node<E>> parentNode = findBy(parent);
+
+        if (parentNode.isPresent()) {
+            Node<E> parentElement = parentNode.get();
+            parentElement.children.add(new Node<>(child));
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> element = data.poll();
+            if (element.value.equals(value)) {
+                result = Optional.of(element);
+                break;
+            }
+            data.addAll(element.children);
+        }
+        return result;
+    }
+}
