a2237bfb4c005928b4ec31d3e4b2b32ea3e3acd6
tawcent <rusx745@gmail.com>
2025-03-11 21:01:59 +0400

    init maven

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,55 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines()
+                    .filter(line -> !line.startsWith("#") && !line.trim().isEmpty())
+                    .forEach(line -> {
+                        String[] parts = line.split("=", 2);
+                        if (parts.length != 2 || parts[0].isEmpty() || parts[1].isEmpty()) {
+                            throw new IllegalArgumentException("Invalid format: " + line);
+                        }
+                        values.put(parts[0], parts[1]); // Сохраняем в Map
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner output = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(output::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config("data/app.properties");
+        config.load();
+        System.out.println(config);
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.username")).isEqualTo("postgres");
+    }
+
+    @Test
+    void whenPairWithComment() {
+        String path = "data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("assas")).isEqualTo("qwwwq");
+    }
+
+    @Test
+    void whenPairError() {
+        String path = "data/pair.error.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("value");
+    }
+}
