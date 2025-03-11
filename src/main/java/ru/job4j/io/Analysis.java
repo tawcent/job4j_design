facbc1f14bee85c0d4073588af1649ed6665bd65
tawcent <rusx745@gmail.com>
2025-03-11 22:32:06 +0400

    init maven

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,4 +1,38 @@
 package ru.job4j.io;
 
+import java.io.*;
+
 public class Analysis {
+
+    public void unavailable(String source, String target) {
+        boolean serverDown = false;
+        String start = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(new FileWriter(target))) {
+
+                 String line;
+                 while ((line = reader.readLine()) != null) {
+                     String[] parts = line.split(" ");
+                     String status = parts[0];
+                     String time = parts[1];
+
+                     if ((status.equals("400") || status.equals("500")) && !serverDown) {
+                         start = time;
+                         serverDown = true;
+                     } else if (status.equals("200") || status.equals("300") && serverDown) {
+                         if (start != null) {
+                             writer.println(start + ";" + time + ";");
+                             serverDown = false;
+                         }
+                     }
+                 }
+             } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
 }
