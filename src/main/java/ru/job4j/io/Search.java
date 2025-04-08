d72a7b3619c5d169b7f44f96c635267be672e9ca
tawcent <rusx745@gmail.com>
2025-04-08 20:35:35 +0400

    init maven

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get("C:\\projects");
+
+        Predicate<Path> condition = path -> path.toFile().getName().endsWith(".js");
+        List<Path> result = search(start, condition);
+
+        result.forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
