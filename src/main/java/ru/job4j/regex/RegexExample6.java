d4222210a40ee5fdd2d798117550c34b573b66ee
tawcent <rusx745@gmail.com>
2025-06-10 22:11:16 +0400

    init maven

---
+++ src/main/java/ru/job4j/regex/RegexExample6.java
@@ -0,0 +1,15 @@
+package ru.job4j.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexExample6 {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("Job4j");
+        String text = "Job4j1 и Job4j2 и Job4j3";
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+    }
+}
